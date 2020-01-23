a252377664bd870919882fe586cee82b663c8855
alex-png <alexrod102797@gmail.com>
2020-01-23 18:21:29 -0500

    gave user all beers on getgo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
+            user.beers = Beer.all
             render json: user, status: :created
         else
             render json: user.errors, status: :unprocessable_entity
